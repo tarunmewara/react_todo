59fa2edb3c561cdd02ac27f578027acbc4314cde
tarunmewara <155548066+tarunmewara@users.noreply.github.com>
2024-07-11 02:13:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Create from "./components/Create";
+import Show from "./components/Show";
+
+const App = () => {
+    const [tasks, settasks] = useState(
+        JSON.parse(localStorage.getItem("tasks")) || []
+    );
+
+    return (
+        <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
+            {/*  */}
+            <Header />
+            {/*  */}
+            <Create tasks={tasks} settasks={settasks} />
+            {/*  */}
+            <Show tasks={tasks} settasks={settasks} />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import TasksContext from "./Context/TasksContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <TasksContext>
+        <App />
+    </TasksContext>
+);
